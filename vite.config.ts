9ceaba02571376dbdc288b00b783fe25e4da0562
lingovo-aapp <lingovoapp@gmail.com>
2025-06-17 16:05:12 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',  // Ensure assets are correctly referenced for relative paths
+  build: {
+    outDir: 'dist',  // Ensure Vite outputs the production files to the 'dist' folder
+  },
 })
